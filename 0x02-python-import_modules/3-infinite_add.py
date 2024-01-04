f92e78f2c07746af066f64a7d08396d61d60665a
shaun-saisi <saisishaun4@gmail.com>
2024-01-04 09:17:42 +0000

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 from sys import argv
+if __name__ == "__main__":
 
-# Use a generator expression to sum all integers from command-line arguments
-result = sum(int(arg) for arg in argv[1:])
+    # Use a generator expression to sum all integers from command-line arguments
+    result = sum(int(arg) for arg in argv[1:])
 
-print(result)
+    print(result)
 
