43cd654aa2dc25f16fbab58e0d15fc1b85578234
shaun-saisi <saisishaun4@gmail.com>
2024-01-04 09:19:17 +0000

    hidden files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 import os
-def list_hidden_files(directory='hidden_4.pyc'):
-    # Get a list of all files in the directory
-    all_files = os.listdir(directory)
-
-    # Filter out hidden files (those starting with '.')
-    hidden_files = [file for file in all_files if not file.startswith('.')]
-
-    # Sort the list alphabetically
-    hidden_files.sort()
-
-    # Print one name per line
-    for file in hidden_files:
-        print(file)
-
-# Run the function if the script is executed
 if __name__ == "__main__":
+    def list_hidden_files(directory='hidden_4.pyc'):
+        # Get a list of all files in the directory
+        all_files = os.listdir(directory)
+
+        # Filter out hidden files (those starting with '.')
+        hidden_files = [file for file in all_files if not file.startswith('.')]
+
+        # Sort the list alphabetically
+        hidden_files.sort()
+
+        # Print one name per line
+        for file in hidden_files:
+            print(file)
+
+    # Run the function if the script is executed
+    if __name__ == "__main__":
     list_hidden_files()
