907c8e7229911737665528383bcd63f5fb81dd33
shaun-saisi <saisishaun4@gmail.com>
2024-01-10 11:45:04 +0000

    function to search and replace in alist

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for element in my_list:
+        new_element = replace if element == search else element
+        new_list.append(new_element)
+        return new_listbin/python3
+
+
