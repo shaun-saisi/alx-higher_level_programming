39f6187ace9adc639dd2efaeff971f8bd540c501
shaun-saisi <saisishaun4@gmail.com>
2024-01-24 10:48:10 +0000

    a n instantiated square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" The class square below has a private instance attribute size
+which is instantiated"""
+
+class Square:
+    def __init__(self,size):
+        self.__size = size
