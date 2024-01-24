473e2cce8a718457528346dc47a83bce75b3e549
shaun-saisi <saisishaun4@gmail.com>
2024-01-24 12:51:51 +0000

    ccs

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""defining the class square"""
+
+class Square:
+    """What is comntained in the square
+    Attributes:
+        __size (int): one of the sides of the square
+    """
+
+    def __init__(self, size = 0):
+        """intitializing the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        self.size = size
+
+    def area(self):
+        """Calculates the area ofr the square
+        Returns:
+            The area
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getter of the __size
+        Returns:
+            size ofthe square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of the __size
+        Args:
+            value (int): the size of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("Size must be an integer")
+        else:
+            if value <0:
+                raise ValueError("Size must be greator or equal to 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+	""" prints the square
+	Returns:
+		None
+	"""
+	if self.__size == 0:
+	   print()
+           return
+	for i in range(self.__size):
+            print("".join(["#" for j in range (self.__size)]))
