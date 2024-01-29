0b01bf2ddcf3a94b325a5a375915768fc0c5dc51
shaun-saisi <saisishaun4@gmail.com>
2024-01-29 14:14:31 +0000

    delete attribute

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""ss Rectangle with area and perimeter"""
+
+
+class Rectangle:
+    """Just a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializing the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __del__(self):
+        """Print a delete message"""
+        print("Bye rectangle...")
+
+    def perimeter(self):
+        """The perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def area(self):
+        """The area of the rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """Returns a string"""
+        string = ""
+        if self.__height != 0 and self.__width != 0:
+            string += "\n".join("#" * self.__width for _ in range(self.__height))
+        return string
+
+    def __repr__(self):
+        """Returns a representation of the rectangle"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
