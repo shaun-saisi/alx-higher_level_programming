c34f7b27557f09114f3be836a38f4cb3fcaee5e5
shaun-saisi <saisishaun4@gmail.com>
2024-01-29 13:42:44 +0000

    as a rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""cass Rectangle with area and perimeter"""
+
+
+class Rectangle:
+    """Just a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializing the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for private instance attribute width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the private instance attribute height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def perimeter(self):
+        """The perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def area(self):
+        """The area of the rectangle"""
+        return self.__height * self.__width
+
+   def __str__(self):
+	"""Returns a string"""
+	string = ""
+	if self.__width != 0 and self.__height != 0:
+		for _ in range(self.__height):
+			string +="#" * self.__width + "\n"
+	return string
