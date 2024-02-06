a33ac3aff5cb005f33763c1b26deecc866764c7b
shaun-saisi <saisishaun4@gmail.com>
2024-02-06 12:56:12 +0000

    add to json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,5 +11,10 @@ if __name__ == "__main__":
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+
+    # Extend items with command-line arguments
     items.extend(sys.argv[1:])
+
+    # Save the updated items to the JSON file
     save_to_json_file(items, "add_item.json")
+
